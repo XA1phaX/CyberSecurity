06761a69ac9cf18ee48df1c56b223591a2bc1da5
JT Clarke <jacksonclarke2001@hotmail.com>
2022-10-06 14:58:17 -0600

    signing in with 2 factor

--- manageuser.js
+++ manageuser.js
@@ -3,132 +3,143 @@
 let phonenumber = "";
 let onetimepassword = "";
 let verifypassword = "";
-let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
+let passwordRegEx = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
 
-function setphonenumber(){
-    phonenumber = $("#phonenumber").val();
+function setphonenumber() {
+  phonenumber = $("#phonenumber").val();
 }
 
-function setonetimepassword(){
-    onetimepassword = $("#onetimepassword").val();
+function setonetimepassword() {
+  onetimepassword = $("#onetimepassword").val();
 }
 
-function setverifypassword(){
-    verifypassword = $("#verifypassword").val();
-    if (verifypassword!=onetimepassword){
-        alert('Passwords must be entered the same twice');
-    }
+function setverifypassword() {
+  verifypassword = $("#verifypassword").val();
+  if (verifypassword != onetimepassword) {
+    alert("Passwords must be entered the same twice");
+  }
 }
 
-function savetoken(token){
-// whatever passes as token should save into local storage
-    if (window.localStorage){
-     localStorage.setItem("token", token);
-    }
-
+function savetoken(token) {
+  // whatever passes as token should save into local storage
+  if (window.localStorage) {
+    localStorage.setItem("token", token);
+  }
 }
 
-function checkexpiredtoken(token){
-// read token from local storage - check with ajax call
-    if(window.localStorage){
+function checkexpiredtoken(token) {
+  // read token from local storage - check with ajax call
+  if (window.localStorage) {
     usertoken = localStorage.getItem("token");
     $.ajax({
-       type: 'GET',
-        url: '/validate/'+token,
-        data: JSON.stringify({usertoken}),
-        success: function(data){savetoken(data)},
-        contentType: "application/text",
-        dataType: 'text' })
-    }
+      type: "GET",
+      url: "/validate/" + token,
+      data: JSON.stringify({ usertoken }),
+      success: function (data) {
+        savetoken(data);
+      },
+      contentType: "application/text",
+      dataType: "text",
+    });
+  }
 }
 
-const sendtext =()=> {
+const sendtext = () => {
   setonetimepassword();
   setphonenumber();
   $.ajax({
     type: "POST",
-    url: "https://dev.stedi.me/twofactorlogin/"+phonenumber,
+    url: "https://dev.stedi.me/twofactorlogin/" + phonenumber,
+    contentType: "application/text",
+    dataType: "text",
+  });
+};
+
+function userlogin() {
+  setonetimepassword();
+  setphonenumber();
+  $.ajax({
+    type: "POST",
+    url: "https://dev.stedi.me/twofactorlogin",
+    data: JSON.stringify({
+      phoneNumber: phonenumber,
+      oneTimePassword: onetimepassword,
+    }),
+    success: function (data) {
+      window.location.href = "/timer.html#" + data; //add the token to the url
+    },
     contentType: "application/text",
     dataType: "text",
   });
 }
 
-function userlogin(){
-    setonetimepassword();
-    setphonenumber();
-    $.ajax({
-      type: "POST",
-      url: "https://dev.stedi.me/login",
-      data: JSON.stringify({ phonenumber, onetimepassword }),
-      success: function (data) {
-        window.location.href = "/timer.html#" + data; //add the token to the url
-      },
-      contentType: "application/text",
-      dataType: "text",
-    });
-
-}
-
-function readonlyforms(formid){
-    form = document.getElementById(formid);
-    elements = form.elements;
-    for (i = 0, len = elements.length; i < len; ++i) {
+function readonlyforms(formid) {
+  form = document.getElementById(formid);
+  elements = form.elements;
+  for (i = 0, len = elements.length; i < len; ++i) {
     elements[i].readOnly = true;
-    }
-    createbutton();
+  }
+  createbutton();
 }
- function pwsDisableInput( element, condition ) {
-        if ( condition == true ) {
-            element.disabled = true;
-
-        } else {
-            element.removeAttribute("disabled");
-        }
-
- }
-
-function createbutton(){
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = "OK";
-    button.onclick = window.location.href = "/index.html";
-    context.appendChild(button);
+function pwsDisableInput(element, condition) {
+  if (condition == true) {
+    element.disabled = true;
+  } else {
+    element.removeAttribute("disabled");
+  }
 }
 
-
-function createuser(){
-    $.ajax({
-        type: 'POST',
-        url: '/user',
-        data: JSON.stringify({phonenumber, 'email': phonenumber, onetimepassword, 'verifyPassword': vpwd, 'accountType':'Personal'}),//we are using the email as the user name
-        success: function(data) { alert(data);
-//        readonlyforms("newUser");
-//        alert(readonlyforms("newUser"));
-        window.location.href = "/index.html"},
-        contentType: "application/text",
-        dataType: 'text'
-    });
+function createbutton() {
+  var button = document.createElement("input");
+  button.type = "button";
+  button.value = "OK";
+  button.onclick = window.location.href = "/index.html";
+  context.appendChild(button);
 }
 
-function getstephistory(){
-      $.ajax({
-            type: 'POST',
-            url: '/stephistory',
-            data: JSON.stringify({phonenumber}),
-            success: function(data) { alert(data);
-            json = $.parseJSON(data);
-            $('#results').html(json.name+' Total Steps: ' + json.stepTotal)},
-            contentType: "application/text",
-            dataType: 'text'
-        });
+function createuser() {
+  $.ajax({
+    type: "POST",
+    url: "/user",
+    data: JSON.stringify({
+      phonenumber,
+      email: phonenumber,
+      onetimepassword,
+      verifyPassword: vpwd,
+      accountType: "Personal",
+    }), //we are using the email as the user name
+    success: function (data) {
+      alert(data);
+      //        readonlyforms("newUser");
+      //        alert(readonlyforms("newUser"));
+      window.location.href = "/index.html";
+    },
+    contentType: "application/text",
+    dataType: "text",
+  });
 }
 
-var enterFunction = (event) =>{
-    if (event.keyCode === 13){
-        event.preventDefault();
-        $("#loginbtn").click();
-    }
+function getstephistory() {
+  $.ajax({
+    type: "POST",
+    url: "/stephistory",
+    data: JSON.stringify({ phonenumber }),
+    success: function (data) {
+      alert(data);
+      json = $.parseJSON(data);
+      $("#results").html(json.name + " Total Steps: " + json.stepTotal);
+    },
+    contentType: "application/text",
+    dataType: "text",
+  });
 }
+
+var enterFunction = (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    $("#loginbtn").click();
+  }
+};
 
 var passwordField = document.getElementById("onetimepassword");
 
